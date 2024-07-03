87c6fe0f0f4b00590ef7f96245067266c36a4b12
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-07-03 15:45:13 -0300

    Inclusão de edição e exclusão de eventos.

--- agenda/urls.py
+++ agenda/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>', views.delete_evento),
     path('local/<titulo_evento>/', views.local),
     #path('', views.index ),
     path('', RedirectView.as_view(url='/agenda/')),
