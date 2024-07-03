95516d98acfcac9d44ff7bca497930c82594197d
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-07-02 23:59:19 -0300

    Alterações de lista de evento, criação de templates.

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+
 
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agenda/', views.lista_eventos),
     path('local/<titulo_evento>/', views.local),
+    #path('', views.index )
+    path('', RedirectView.as_view(url='/agenda/'))
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 
 from core.models import Evento
 
 # Create your views here.
+
+#def index(request):
+#    return redirect('/agenda/')
 
 def local(request, titulo_evento):
     try:
@@ -11,3 +14,9 @@ def local(request, titulo_evento):
         return HttpResponse('O local do evento {} sera no {}. '.format(titulo_evento, local))
     except:
         return HttpResponse('Não foi possível realizar a consulta')
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+
+    return render(request, 'agenda.html', dados)
