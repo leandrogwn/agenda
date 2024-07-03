6464e138bfc27f80106de49542feed6159812099
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-07-03 02:50:29 -0300

    Inclusão de authenticate, login e logout.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from core.models import Evento
 
@@ -6,6 +9,26 @@ from core.models import Evento
 
 #def index(request):
 #    return redirect('/agenda/')
+
+def login_user(request):
+    return render(request,'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request,"Usuário ou senha inválida.")
+    return redirect('/')
 
 def local(request, titulo_evento):
     try:
@@ -15,8 +38,10 @@ def local(request, titulo_evento):
     except:
         return HttpResponse('Não foi possível realizar a consulta')
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    user = request.user
+    evento = Evento.objects.filter(usuario=user)
     dados = {'eventos': evento}
 
     return render(request, 'agenda.html', dados)
